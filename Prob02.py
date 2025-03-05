8563d591af39d660be5f97ce1e0618eb6b1cb0cc
Kristina Cas <m.kristinacas@gmail.com>
2025-03-05 14:34:23 +0800

    Prob02 Done

---
+++ Prob02.py
@@ -0,0 +1,10 @@
+# Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
+num1 = float(input("First number: "))
+num2 = float(input("Second number: "))
+
+if num1 != num2:
+    print("Not Equal")
+else:
+    print("Equal")
+    
