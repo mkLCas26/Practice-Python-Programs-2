97cff84df8782ad8856064cbdee874e4e23de756
Kristina Cas <m.kristinacas@gmail.com>
2025-03-05 14:24:16 +0800

    Prob01 Done

---
+++ Prob01.py
@@ -0,0 +1,10 @@
+# Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
+
+num1 = float(input("First number: "))
+num2 = float(input("Second number: "))
+
+if num1 < num2:
+    print(num1)
+else:
+    print(num2)
+    
