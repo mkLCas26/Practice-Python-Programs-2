8a8fbef98c93e56a8e49b1a0cd5fe6f164a20896
Kristina Cas <m.kristinacas@gmail.com>
2025-03-07 23:24:58 +0800

    Changes

--- Prob06.py
+++ Prob06.py
@@ -1,14 +1,12 @@
 # Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 
 num1 = float(input("Enter 10 numbers: "))
-remNum = 0
 counter = 0
 
 while counter < 9:
     counter += 1
     nums = float(input("Enter 10 numbers: "))
-    remNum += nums
+    num1 -= nums
 
-diff = num1 - remNum
-print(diff)
+print(num1)
     
